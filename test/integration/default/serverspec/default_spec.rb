97d23de7ff12b81073ca5cbe92364cb2ac78877e
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-11 14:34:21 -0700

    Include tests for '.ssh/authorized_keys'

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -6,4 +6,13 @@ describe 'users::default' do
         it { should belong_to_group 'test' }
         it { should have_home_directory "/home/test" }
     end
+
+    describe file('/home/test/.ssh/authorized_keys') do
+        it { should be_file }
+        it { should exist }
+        it { should contain 'ssh-rsa TEST-KEY' }
+        it { should be_mode 600 }
+        it { should be_owned_by 'test' }
+        it { should be_grouped_into 'test' }
+    end
 end
