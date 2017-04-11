e739cca1fda0e9ce67650ecbac85d20e5eae2d99
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-04-11 12:34:47 -0700

    Metadata

---
+++ metadata.rb
@@ -0,0 +1,14 @@
+name                'cop_users'
+description         'Creates user accounts'
+long_description    IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+license             'MIT'
+maintainer          'Copious, Inc.'
+maintainer_email    'engineering@copiousinc.com'
+version             '0.0.0'
+source_url          'https://github.com/copious-cookbooks/users'
+issues_url          'https://github.com/copious-cookbooks/users/issues'
+
+supports 'ubuntu', '>= 14.04'
+supports 'debian', '>= 6'
+supports 'rhel', '>= 6'
+supports 'centos', '>= 6'
